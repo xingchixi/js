2adf78648cecb9ef2017043136561093dad62014
xingchixi <xxingchi@yahoo.com>
2022-03-02 18:13:41 -0800

    Create world.jsx

---
+++ react/world.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+
+class World extends React.Component {
+  render() {
+      return <div><h1>World 1</h1></div>
+  }
+}
+
+export { World }; //export default World;
+    
+//ReactDOM.render(<World/>, document.getElementById('world'));
