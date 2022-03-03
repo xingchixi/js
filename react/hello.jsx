fe2ab826e75dd5806639e270ee705a97e2aa4ba4
xingchixi <xxingchi@yahoo.com>
2022-03-02 18:12:28 -0800

    Update hello.jsx

--- react/hello.jsx
+++ react/hello.jsx
@@ -1,9 +1,16 @@
+//https://www.twilio.com/blog/2015/08/setting-up-react-for-es6-with-webpack-and-babel-2.html
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { Hello } from './hello.jsx';
-import { World } from './world.jsx';
+//import ReactDOM from 'react-dom';
  
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello2, </h1>
+  }
+}
 
-ReactDOM.render(<Hello/>, document.getElementById('hello'));
-ReactDOM.render(<World/>, document.getElementById('world'));
+
+
+export { Hello }; //export default Hello;
+
+//ReactDOM.render(<Hello/>, document.getElementById('hello'));
